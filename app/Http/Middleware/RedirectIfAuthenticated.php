ac27143387b649adad80dc9054d3450a70f11cec
at-tienpd <tienpd@asiantech.vn>
2017-01-03 11:48:32 +0700

    Update circle

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,9 +10,10 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param \Illuminate\Http\Request $request request
+     * @param \Closure                 $next    next
+     * @param string|null              $guard   guard
+     *
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
