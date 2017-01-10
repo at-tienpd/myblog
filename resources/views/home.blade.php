04c46a9acedb81532fbc1bdc3e26546c7754e367
at-tienpd <tienpd@asiantech.vn>
2017-01-10 08:43:48 +0700

    Config lang file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">{{ trans('home.panel_heading') }}</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    {{ trans('home.logged') }}
                 </div>
             </div>
         </div>
