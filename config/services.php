deb8de83d798ac818248481a7cd7086b7c7fbc56
at-tienpd <tienpd@asiantech.vn>
2017-01-11 15:49:09 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -35,14 +35,14 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
     'facebook' => [
-    'client_id' => '1789892884608521', // configure with your app id
-    'client_secret' => 'dcde370fa812f9e21cf973fc162167f8', // your app secret
-    'redirect' => 'http://localhost/callback/facebook', // leave blank for now
+        'client_id' => '1789892884608521',
+        'client_secret' => 'dcde370fa812f9e21cf973fc162167f8',
+        'redirect' => env('APP_URL').'/callback/facebook',
     ],
     'twitter' => [
-    'client_id' => '4Od7zaF0wVskWAyW6mn5jFlH0', // configure with your app id
-    'client_secret' => 'r8kZg8TCGEjUytWSLzY86C1qSH0VqITgm5y6zWrOCHypTReJWN', // your app secret
-    'redirect' => 'http://localhost/callback/twitter', // leave blank for now
+        'client_id' => '4Od7zaF0wVskWAyW6mn5jFlH0',
+        'client_secret' => 'r8kZg8TCGEjUytWSLzY86C1qSH0VqITgm5y6zWrOCHypTReJWN',
+        'redirect' => env('APP_URL').'/callback/twitter',
     ],
 
 ];
