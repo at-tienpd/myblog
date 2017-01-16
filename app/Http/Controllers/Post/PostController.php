d0b60fd1544a035630851233dcaeaa320eecbfd2
at-tienpd <tienpd@asiantech.vn>
2017-01-16 09:56:18 +0700

    Delete method not use

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -52,16 +52,6 @@ class PostController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
