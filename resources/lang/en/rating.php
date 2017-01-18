b6e7e113ce297a8285aefe0d0f7d25030eb08385
at-tienpd <tienpd@asiantech.vn>
2017-01-18 15:36:04 +0700

    Feature#34

--- resources/lang/en/rating.php
+++ resources/lang/en/rating.php
@@ -16,4 +16,18 @@ return [
     'button'=> [
         'store' => 'Submit',
     ],
+    'point'=> [
+        '1' => 'Sucks big time - 1 star',
+        '2' => 'Kinda bad - 2 stars',
+        '3' => 'Meh - 3 stars',
+        '4' => 'Pretty good - 4 stars',
+        '5' => 'Awesome - 5 stars',
+    ],
+    'value'=> [
+        '1' => '1',
+        '2' => '2',
+        '3' => '3',
+        '4' => '4',
+        '5' => '5',
+    ],
 ];
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -42,11 +42,11 @@
           <form action="{{ route('ratings.store') }}" method="post">
           {{ csrf_field() }}
           <fieldset class="rating">
-              <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-              <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-              <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-              <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-              <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+              <input type="radio" id="star5" name="rating" value="{{ trans('rating.value.5') }}" /><label class = "full" for="star5" title="{{ trans('rating.point.5') }}"></label>
+              <input type="radio" id="star4" name="rating" value="{{ trans('rating.value.4') }}" /><label class = "full" for="star4" title="{{ trans('rating.point.4') }}"></label>
+              <input type="radio" id="star3" name="rating" value="{{ trans('rating.value.3') }}" /><label class = "full" for="star3" title="{{ trans('rating.point.3') }}"></label>
+              <input type="radio" id="star2" name="rating" value="{{ trans('rating.value.2') }}" /><label class = "full" for="star2" title="{{ trans('rating.point.2') }}"></label>
+              <input type="radio" id="star1" name="rating" value="{{ trans('rating.value.1') }}" /><label class = "full" for="star1" title="{{ trans('rating.point.1') }}"></label>
               <input type="hidden" value="{{ $post->id }}" name="post_id">
           </fieldset>
 
